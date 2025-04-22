2227ff16460f831d515527346faf52b892b99cd6
ibrahimnassar31 <85653398+ibrahimnassar31@users.noreply.github.com>
2025-04-22 12:20:37 +0300

    rename port

--- server.js
+++ server.js
@@ -50,7 +50,7 @@ app.use(limiter);
 connectDB();
 
 // تشغيل السيرفر
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 server.listen(PORT, () => {
   console.log(`✅ Server is running on port ${PORT}`);
 });
